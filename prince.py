7beed80cdd524afa5c7d7edef592ef5ea3e1e0fe
Prince9558 <119520771+Prince9558@users.noreply.github.com>
2022-12-03 15:23:31 +0530

    Add files via upload

---
+++ prince.py
@@ -0,0 +1,37 @@
+# Import Math Module
+# Write a program to calculate the hypotenuse of the right angled triangle 
+
+'''import math
+base=int(input("Enter the base of right angled triangle: "))
+height=int(input("Enter the height of right angled triangle: "))
+hypotenuse=math.sqrt(base*base+height*height)
+print("Hypotenuse: ",hypotenuse)
+
+a=ord("A")
+b=ord("Z")
+c=ord("a")
+d=ord("z")
+print(a)
+print(b)
+print(c)
+print(d)
+e=chr(90)
+f=chr(65)
+g=chr(97)
+h=chr(122)
+print(e)
+print(f)
+print(g)
+print(h)
+
+# Write a program to calculate simple interest.
+Principle=int(input("principle:: "))
+ROI=eval(input("rate of interest: "))
+Time=eval(input("number of years: "))
+SI=Principle*ROI*Time/100
+print("Simple Interest: ",SI)'''
+
+#Write a program to read a temperature in celsius from the user and convert it into Fahrenheit
+a=eval(input("Celsius: "))
+b=(9/5)*a+32
+print("Fahrenheit: ",b)
