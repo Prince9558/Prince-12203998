0ac8d26b3b5a7b61ede2b3926728f817fc84c28b
Prince Raj <119520771+Prince9558@users.noreply.github.com>
2022-12-31 20:56:52 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,23 @@
+x = lambda a: a + 10
+print(x(5))   
+
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11)) 
+print(mytripler(11))
+
